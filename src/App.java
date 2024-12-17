01cd138adbb91dff8a13e66d030247ebaf04456e
Novotny_Filip <novof107@hsd.k12.or.us>
2024-12-17 09:46:08 -0800

    adding base code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,73 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main(String[] args) throws Exception 
+    {      
+    Scanner input = new Scanner(System.in);
+            
+    System.out.println("Name: ");
+    String theName = input.nextLine();
+
+    System.out.println("Age: ");
+    int theAge = input.nextInt();
+
+    System.out.println("Funds: ");
+    int theFunds = input.nextInt();
+    
+    System.out.println("Casino name: ");
+    String casinoName = input.nextLine();
+
+    input.nextLine();
+
+    System.out.println("Casino funds: ");
+    int casinoFunds = input.nextInt();
+    
+    
+    class Raccoon
+        {
+            private String name;
+            private int age;
+            private int funds;
+
+
+            public Raccoon(String theName, int theAge, int theFunds)
+            {
+                name = theName;
+                age = theAge;
+                funds = theFunds;
+            }
+
+            public String toString()
+            {
+                String res = "Racoon called " + name + " is " + age + " years old and has " + funds + " shillings to his name";
+                return res;
+            }
+        }
+
+        class Casino
+        {
+            private String cname;
+            private int wealth;
+
+            public Casino(String casinoName, int casinoFunds) 
+            {
+                cname = casinoName;
+                wealth = casinoFunds;
+            }
+
+            public String toString() 
+            {
+                String res = "Casino " + cname + " has " + wealth + " shillings at their disposal";
+                return res;
+            }
+        }
+    Raccoon example = new Raccoon(theName, theAge, theFunds);
+
+    System.out.println(example);
+
+    Casino beans = new Casino(casinoName, casinoFunds);
+
+    System.out.println(beans);
     }
 }
